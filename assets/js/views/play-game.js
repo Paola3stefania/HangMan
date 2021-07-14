a56693800aec446c14514277658ed22a57b0c83a
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-07-14 15:31:04 +0200

    username view

---
+++ assets/js/views/play-game.js
@@ -0,0 +1,4 @@
+//remove event Listener before deleting de node
+
+document.querySelector("link_start").removeEventListener("click", startGame);
+document.querySelector("link_ranking").removeEventListener("click", ranking);
--- assets/js/views/register-user.js
+++ assets/js/views/register-user.js
@@ -1,5 +1,7 @@
 //*TODO import {templateUsername} from "";
 
+//*TODO import {startGame} from "play-game.js";
+
 
 let usuario={
     id:0,
@@ -13,15 +15,36 @@ let usuario={
 //register user its first screen, so it charges on load.
 
 //? shows the view of the username
-
+//*TODO <template id=???></template> -- username
 
 function userRegistration(){
 
+    let wrapper=document.getElementById("app");
     //make sure that the page its empty before doing nothing
-    document.getElementById("app").innerHTML="";
+    wrapper.innerHTML="";
 
     //clone the template of username registration
+    wrapper.insertAdjacentHTML("beforeend", templateUsername);
+
+	let usernameNode = document.getElementById("username").content;
+	let copyNode = document.importNode(usernameNode, true);
+
+    //delete de template from the html 
+    wrapper.innerHTML="";
+
+	wrapper.appendChild(copyNode);
+
+    //botones event listener: start & ranking
+    //*TODO startGame .. como la llamo Gonza??
+
+	document.querySelector("link_start").addEventListener("click", startGame);
+    document.querySelector("link_ranking").addEventListener("click", ranking);
+
+    //ranking funcion que vaya a cada vista segun la eleccion de easy medium hard
+
 
 
 }
+
+
 
