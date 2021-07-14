6483ffe896d4487358af45f5813eca2a2259fae6
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-07-14 15:04:16 +0200

    editing register view

--- assets/js/views/register-user.js
+++ assets/js/views/register-user.js
@@ -1,1 +1,27 @@
 //*TODO import {templateUsername} from "";
+
+
+let usuario={
+    id:0,
+    name:"",
+    score:0,
+    state:"",
+    timePlayed=0,
+    gameLevel=""
+}
+
+//register user its first screen, so it charges on load.
+
+//? shows the view of the username
+
+
+function userRegistration(){
+
+    //make sure that the page its empty before doing nothing
+    document.getElementById("app").innerHTML="";
+
+    //clone the template of username registration
+
+
+}
+
