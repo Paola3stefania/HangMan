819f3505d43ee18452cc7c9c0e63aaa5a36b1b8c
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-07-15 18:58:26 +0200

    RemoveListeners

--- assets/js/views/register-user.js
+++ assets/js/views/register-user.js
@@ -3,68 +3,71 @@ import { wrapper } from "../main.js";
 import { username } from "../templates.js";
 
 let usuario = {
-	id: 0,
-	name: "",
-	score: 0,
-	state: "",
-	timePlayed: 0,
-	gameLevel: "",
+  id: 0,
+  name: "",
+  score: 0,
+  state: "",
+  timePlayed: 0,
+  gameLevel: "",
 };
 console.log("hola");
 //register user its first screen, so it charges on load.
 //? shows the view of the username
 
 function userRegistration() {
-	//make sure that the page its empty before doing nothing
-	wrapper.innerHTML = "";
+  document
+    .querySelector(".main__link-username")
+    .removeEventListener("click", goToUsername);
+  //make sure that the page its empty before doing nothing
+  wrapper.innerHTML = "";
 
-	//insert the template HTML in the main.html div app
-	let templateUsername = username;
-	wrapper.insertAdjacentHTML("beforeend", templateUsername);
+  //insert the template HTML in the main.html div app
+  let templateUsername = username;
+  wrapper.insertAdjacentHTML("beforeend", templateUsername);
 
-	//clone the template of username registration
-	let usernameNode = document.getElementById("username").content;
-	let copyNode = document.importNode(usernameNode, true);
+  //clone the template of username registration
+  let usernameNode = document.getElementById("username").content;
+  let copyNode = document.importNode(usernameNode, true);
 
-	//delete de template from the html
-	wrapper.innerHTML = "";
+  //delete de template from the html
+  wrapper.innerHTML = "";
 
-	// insert the Node of the username
-	wrapper.appendChild(copyNode);
+  // insert the Node of the username
+  wrapper.appendChild(copyNode);
 
-	//botones event listener: start & ranking
-	document.getElementById("link-start").addEventListener("click", goToGame);
-	document
-		.getElementById("link-ranking")
-		.addEventListener("click", goToRanking);
+  //botones event listener: start & ranking
+  document.getElementById("link-start").addEventListener("click", goToGame);
+  document
+    .getElementById("link-ranking")
+    .addEventListener("click", goToRanking);
 
-	//save the game level
-	//usuario.gameLevel = document.getElementById("main__input-username").value;
+  //save the game level
+  //usuario.gameLevel = document.getElementById("main__input-username").value;
 
-	//validate name
-	//*TODO maxlenght, just letters
-	// necesito un campo de error tipo span para el USUARIO
+  //validate name
+  //*TODO maxlenght, just letters
+  // necesito un campo de error tipo span para el USUARIO
 
-	const usernameRegexp = /^[a-z0-9]+$/i;
-	const usernameInput = document.getElementById("username-value");
+  const usernameRegexp = /^[a-z0-9]+$/i;
+  const usernameInput = document.getElementById("username-value");
 
-	usernameInput.addEventListener("change", function () {
-		const usernameInputError = document.getElementById("username-validation");
-		const test = usernameRegexp.test(usernameInput.value);
+  usernameInput.addEventListener("change", function () {
+    const usernameInputError = document.getElementById("username-validation");
+    const test = usernameRegexp.test(usernameInput.value);
 
-		if (test) {
-			usernameInputError.innerHTML = "";
+    if (test) {
+      usernameInputError.innerHTML = "";
 
-			usernameInput.classList.remove("invalid");
-			usuario.name = usernameInput.value;
-		} else {
-			usernameInput.classList.add("invalid");
-			usernameInputError.innerHTML = "Insert valid username";
-		}
-	});
+      usernameInput.classList.remove("invalid");
+      usuario.name = usernameInput.value;
+    } else {
+      usernameInput.classList.add("invalid");
+      usernameInputError.innerHTML = "Insert valid username";
+    }
+  });
 
-	//*TODO validate user name
-	//<input id="main__input-username" />;
+  //*TODO validate user name
+  //<input id="main__input-username" />;
 }
 
 export { userRegistration, usuario };
