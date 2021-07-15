a9320cc2f366a5f4eca08de0993712961a6a12eb
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-07-15 18:24:34 +0200

    Finishing counters & Template game keyboard

--- assets/js/counters.js
+++ assets/js/counters.js
@@ -1,12 +1,19 @@
-let nextWordCoundtdown = 5;
+/* IMPORTS */
+
+// import { goToGameOver } from './router.js';
+
+/* COUNTERS FUNCTIONALITY */
+
+/* NEXT WORD COUNTDOWN */
 
 function nextPageTimer() {
+  let nextWordCoundtdown = 5;
   setInterval(function() {
     if(nextWordCoundtdown <= 0){
       document.getElementById("countdown-to-next").innerHTML = "Next!";
-      // TODO - Show next word
+      // TODO - Show next word function
     } else {
-      document.getElementById("countdown-to-next").innerHTML = `${nextWordCoundtdown} seconds remaining`;
+      document.getElementById("countdown-to-next").innerHTML = `Prepare yourself: ${nextWordCoundtdown} seconds remaining`;
     }
     nextWordCoundtdown -= 1;
   }, 1000);
@@ -14,15 +21,15 @@ function nextPageTimer() {
 
 nextPageTimer()
 
-
-let gameOverCountdown = 20;
+/* GAME OVER COUNTDOWN */
 
 // TODO - Change color when it's close to 0
 function gameOverTimer() {
+  let gameOverCountdown = 20;
   setInterval(function() {
     if(gameOverCountdown <= 0){
       document.getElementById("countdown-to-end").innerHTML = "You lost!";
-      // TODO - Show Game over template
+      // goToGameOver(event)
     } else {
       document.getElementById("countdown-to-end").innerHTML = gameOverCountdown;
     }
@@ -31,3 +38,21 @@ function gameOverTimer() {
 }
 
 gameOverTimer()
+
+/* TOTAL PLAY TIME */
+
+let timeStart
+let timeEnd
+let totalPlayedTime
+
+function startingTime() {
+  timeStart = Date.now();
+}
+
+function endingTime() {
+  timeEnd = Date.now();
+  totalPlayedTime = timeEnd - timeStart
+}
+
+startingTime()
+endingTime()
