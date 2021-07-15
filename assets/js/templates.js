777fb33c678ac7dbd8a09ab54756e4db7698a79c
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-07-15 13:37:39 +0200

    Templates with Container

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -1,6 +1,7 @@
 // --------------------------------------------USERNAME SCREEN----------------------------------------
 let username = `
 <template id="username">
+    <div class="main__container">
       <img src="/favicon.svg" alt="" />
       <h1 class="main__title-game">HANGMAN</h1>
       <h2 class="main__title-username">INSERT USERNAME</h2>
@@ -19,12 +20,14 @@ let username = `
       </div>
       <a href="#start" class="main__link-start">START</a>
       <a href="#ranking" class="main__link-ranking">RANKING</a>
+    </div>
 </template>`;
 
 // --------------------------------------------RANKING SCREEN----------------------------------------
 
 let ranking = `
 <template id="ranking">
+    <div class="main__container">
       <img src="/favicon.svg" alt="" />
       <h1 class="main__title-game">HANGMAN</h1>
       <h2 class="main__title-username">BEST PLAYERS</h2>
@@ -44,6 +47,7 @@ let ranking = `
       </section>
       <a href="#username" class="main__link-username">GO BACK</a>
       <a href="#start" class="main__link-start">START</a>
+    </div>
 </template>
 `;
 
@@ -51,7 +55,8 @@ let ranking = `
 
 let game = `
 <template id="game-screen">
-<div class="container__div-header">
+    <div class="main__container">
+      <div class="container__div-header">
         <img src="" alt="Clock">
         <h2>USERNAME</h2>
         <h2>PUCTUATION</h2>
@@ -62,6 +67,7 @@ let game = `
       <div class="container__div-header-keyboard"></div>
       <h2>HINT</h2>
       <a href="#username" class="main__link-username">RESET</a>
+    </div>
 </template>
 `;
 
@@ -69,6 +75,7 @@ let game = `
 
 let youWon = `
 <template id="you-won">
+  <div class="main__container">
     <img src="/favicon.svg" alt="" />
     <h1 class="main__title-game">HANGMAN</h1>
     <h2>USERNAME, YOU BEAT THE GAME</h2>
@@ -89,6 +96,7 @@ let youWon = `
         </ol>
     </div>
     <a href="#username" class="main__link-username">PLAY AGAIN</a>
+  </div>
 </template>
 `;
 
@@ -96,6 +104,7 @@ let youWon = `
 
 let gameOver = `
 <template id="game-over">
+  <div class="main__container">
     <img src="/favicon.svg" alt="" />
     <h1 class="main__title-game">HANGMAN</h1>
     <h2>NICE TRY, USERNAME</h2>
@@ -116,6 +125,7 @@ let gameOver = `
         </ol>
     </div>
     <a href="#username" class="main__link-username">PLAY AGAIN</a>
+  </div>
 </template>
 `;
 
