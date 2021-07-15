c8b6e2bec7feff05cd47e3ee80dd91ef6b44b4db
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-07-15 13:19:22 +0200

    First tries of word logic

---
+++ assets/js/word.js
@@ -0,0 +1,49 @@
+/* IMPORTS */
+
+import { fourLetter, fiveLetter, sixLetter, sevenLetter, eightLetter, nineLetter, wordsInPlay } from './wordsDatabase.js';
+
+/* WORD FUNCTIONALITY */
+
+function getRandomNumber(minValue, maxValue) {
+  let amountNumbers = (maxValue - minValue) + 1; // We declarate how many numbers will be in our calculation
+  let floatNumber = Math.random() * amountNumbers; // We get a random float number (0 to 1) and multiply by our amount
+  let result = Math.floor(floatNumber) + minValue; // We round our number, and avoid values below our range adding the min
+  return result;
+}
+
+let currentLetters = 4 // We declare how many letters our current word has
+let currentGroup
+
+function getGroupOfWords() { // We select the group of words by the amount of letters of currentLetters
+  let group = wordsInPlay[currentLetters];
+  console.log(group)
+  currentGroup = group
+  return group;
+}
+
+function getRandomWord() { // To select a random word from our group, we need a random number from 1 to 10 (amount of words in each group)
+  let position = getRandomNumber(1,10); // Max number, if we want escalation, should be a variable that measures the max key in the object
+  return group[position];
+}
+
+const DISPLAYWORD = document.getElementById('display-word') // We look for the element in the HTML where we are going to print our word
+
+function displayWord() {
+  for (let i = 0; i < testWord.length; i++) {
+    let element = document.createElement('span');
+    element.innerHTML = testWord[i];
+    element.style.textDecoration = 'underline';
+    element.style.paddingLeft = '10px';
+    DISPLAYWORD.appendChild(element);
+  } return
+}
+
+console.log("Hola Paola")
+
+function test() {
+  console.log("Esto es una prueba")
+}
+
+// /* EXPORTS */
+
+export { test };
--- assets/js/wordsDatabase.js
+++ assets/js/wordsDatabase.js
@@ -3,81 +3,81 @@
 /* Al hints are definitions by the Cambridge Dictionary: https://dictionary.cambridge.org */
 
 let fourLetter = {
-  1: {word: 'door', type: 'noun', cathegory: '', hint: 'The part of a building or room that you open or close to get inside it or out of it', length: '4'},
-  2: {word: 'cafe', type: 'noun', cathegory: '', hint: 'A small restaurant where you buy drinks and small meals', length: '4'},
-  3: {word: 'walk', type: 'verb', cathegory: '', hint: 'To move forward by putting one foot in front of the other and then repeating the action', length: '4'},
-  4: {word: 'loop', type: 'noun', cathegory: '', hint: 'A circle of something long and thin, such as a piece of string or wire', length: '4'},
-  5: {word: 'fate', type: 'noun', cathegory: '', hint: 'A power that some people believe controls what will happen', length: '4'},
-  6: {word: 'cold', type: 'adjective', cathegory: '', hint: 'Having a low temperature', length: '4'},
-  7: {word: 'poem', type: 'noun', cathegory: '', hint: 'A piece of writing, especially one that has short lines and uses words that rhyme', length: '4'},
-  8: {word: 'silk', type: 'noun', cathegory: '', hint: 'A type of cloth that is light and smooth', length: '4'},
-  9: {word: 'bear', type: 'noun', cathegory: '', hint: 'A large, strong wild animal with thick fur', length: '4'},
-  10: {word: 'safe', type: 'adjective', cathegory: '', hint: 'Not dangerous', length: '4'}
+  1: {word: 'door', type: 'noun', cathegory: '', hint: 'The part of a building or room that you open or close to get inside it or out of it', length: '4', displayed: false},
+  2: {word: 'cafe', type: 'noun', cathegory: '', hint: 'A small restaurant where you buy drinks and small meals', length: '4', displayed: false},
+  3: {word: 'walk', type: 'verb', cathegory: '', hint: 'To move forward by putting one foot in front of the other and then repeating the action', length: '4', displayed: false},
+  4: {word: 'loop', type: 'noun', cathegory: '', hint: 'A circle of something long and thin, such as a piece of string or wire', length: '4', displayed: false},
+  5: {word: 'fate', type: 'noun', cathegory: '', hint: 'A power that some people believe controls what will happen', length: '4', displayed: false},
+  6: {word: 'cold', type: 'adjective', cathegory: '', hint: 'Having a low temperature', length: '4', displayed: false},
+  7: {word: 'poem', type: 'noun', cathegory: '', hint: 'A piece of writing, especially one that has short lines and uses words that rhyme', length: '4', displayed: false},
+  8: {word: 'silk', type: 'noun', cathegory: '', hint: 'A type of cloth that is light and smooth', length: '4', displayed: false},
+  9: {word: 'bear', type: 'noun', cathegory: '', hint: 'A large, strong wild animal with thick fur', length: '4', displayed: false},
+  10: {word: 'safe', type: 'adjective', cathegory: '', hint: 'Not dangerous', length: '4', displayed: false},
 }
 
 let fiveLetter = {
-  1: {word: 'chief', type: 'noun', cathegory: '', hint: 'The leader of a group', length: '5'},
-  2: {word: 'space', type: 'noun', cathegory: '', hint: 'An empty area that is free to be used', length: '5'},
-  3: {word: 'sweet', type: 'adjective', cathegory: '', hint: 'With a taste like sugar', length: '5'},
-  4: {word: 'north', type: 'noun', cathegory: '', hint: 'The direction that is on your left when you face towards the rising sun', length: '5'},
-  5: {word: 'shake', type: 'verb', cathegory: '', hint: 'To make quick, short movements from side to side or up and down', length: '5'},
-  6: {word: 'trust', type: 'noun', cathegory: '', hint: 'To believe that someone is good and honest and will not harm you', length: '5'},
-  7: {word: 'glass', type: 'noun', cathegory: '', hint: 'A hard, clear substance that objects such as windows and bottles are made of', length: '5'},
-  8: {word: 'beard', type: 'noun', cathegory: '', hint: 'The hair that grows on a man’s chin', length: '5'},
-  9: {word: 'crime', type: 'noun', cathegory: '', hint: 'Illegal activities', length: '5'},
-  10: {word: 'rebel', type: 'adjective', cathegory: '', hint: 'Someone who does not accept authority and refuses to obey rules', length: '5'}
+  1: {word: 'chief', type: 'noun', cathegory: '', hint: 'The leader of a group', length: '5', displayed: false},
+  2: {word: 'space', type: 'noun', cathegory: '', hint: 'An empty area that is free to be used', length: '5', displayed: false},
+  3: {word: 'sweet', type: 'adjective', cathegory: '', hint: 'With a taste like sugar', length: '5', displayed: false},
+  4: {word: 'north', type: 'noun', cathegory: '', hint: 'The direction that is on your left when you face towards the rising sun', length: '5', displayed: false},
+  5: {word: 'shake', type: 'verb', cathegory: '', hint: 'To make quick, short movements from side to side or up and down', length: '5', displayed: false},
+  6: {word: 'trust', type: 'noun', cathegory: '', hint: 'To believe that someone is good and honest and will not harm you', length: '5', displayed: false},
+  7: {word: 'glass', type: 'noun', cathegory: '', hint: 'A hard, clear substance that objects such as windows and bottles are made of', length: '5', displayed: false},
+  8: {word: 'beard', type: 'noun', cathegory: '', hint: 'The hair that grows on a man’s chin', length: '5', displayed: false},
+  9: {word: 'crime', type: 'noun', cathegory: '', hint: 'Illegal activities', length: '5', displayed: false},
+  10: {word: 'rebel', type: 'adjective', cathegory: '', hint: 'Someone who does not accept authority and refuses to obey rules', length: '5', displayed: false},
 }
 
 let sixLetter = {
-  1: {word: 'palace', type: 'noun', cathegory: '', hint: 'A large house where a king or queen lives', length: '6'},
-  2: {word: 'circle', type: 'noun', cathegory: '', hint: 'A round, flat shape like the letter O', length: '6'},
-  3: {word: 'bridge', type: 'noun', cathegory: '', hint: 'A structure that is built over a river or road so that people can go across it', length: '6'},
-  4: {word: 'please', type: 'exclamation', cathegory: '', hint: 'Something that you say to be polite when you are asking for something', length: '6'},
-  5: {word: 'prince', type: 'noun', cathegory: '', hint: 'The son of a king or queen, or one of their close male relatives', length: '6'},
-  6: {word: 'global', type: 'adjective', cathegory: '', hint: 'Relating to the whole world', length: '6'},
-  7: {word: 'polite', type: 'adjective', cathegory: '', hint: 'Behaving in a way that is not rude and shows that you think about other people', length: '6'},
-  8: {word: 'branch', type: 'noun', cathegory: '', hint: 'One of the many parts of a tree that grow out from the main part', length: '6'},
-  9: {word: 'letter', type: 'noun', cathegory: '', hint: 'Some writing that you send to someone, usually by post', length: '6'},
-  10: {word: 'person', type: 'noun', cathegory: '', hint: 'A human being', length: '6'}
+  1: {word: 'palace', type: 'noun', cathegory: '', hint: 'A large house where a king or queen lives', length: '6', displayed: false},
+  2: {word: 'circle', type: 'noun', cathegory: '', hint: 'A round, flat shape like the letter O', length: '6', displayed: false},
+  3: {word: 'bridge', type: 'noun', cathegory: '', hint: 'A structure that is built over a river or road so that people can go across it', length: '6', displayed: false},
+  4: {word: 'please', type: 'exclamation', cathegory: '', hint: 'Something that you say to be polite when you are asking for something', length: '6', displayed: false},
+  5: {word: 'prince', type: 'noun', cathegory: '', hint: 'The son of a king or queen, or one of their close male relatives', length: '6', displayed: false},
+  6: {word: 'global', type: 'adjective', cathegory: '', hint: 'Relating to the whole world', length: '6', displayed: false},
+  7: {word: 'polite', type: 'adjective', cathegory: '', hint: 'Behaving in a way that is not rude and shows that you think about other people', length: '6', displayed: false},
+  8: {word: 'branch', type: 'noun', cathegory: '', hint: 'One of the many parts of a tree that grow out from the main part', length: '6', displayed: false},
+  9: {word: 'letter', type: 'noun', cathegory: '', hint: 'Some writing that you send to someone, usually by post', length: '6', displayed: false},
+  10: {word: 'person', type: 'noun', cathegory: '', hint: 'A human being', length: '6', displayed: false},
 }
 
 let sevenLetter = {
-  1: {word: 'economy', type: 'noun', cathegory: '', hint: 'The system by which a country makes and uses goods and money', length: '7'},
-  2: {word: 'process', type: 'noun', cathegory: '', hint: 'A series of actions that you take in order to do something', length: '7'},
-  3: {word: 'premium', type: 'adjective', cathegory: '', hint: 'Used to refer to something that is of higher than usual quality', length: '7'},
-  4: {word: 'extreme', type: 'adjective', cathegory: '', hint: 'At the furthest point of something', length: '7'},
-  5: {word: 'alcohol', type: 'noun', cathegory: '', hint: 'Drinks such as wine and beer that can make you drunk', length: '7'},
-  6: {word: 'context', type: 'noun', cathegory: '', hint: 'All the facts, opinions, etc. relating to a particular thing or event', length: '7'},
-  7: {word: 'fitness', type: 'noun', cathegory: '', hint: 'The condition of being physically strong and healthy', length: '7'},
-  8: {word: 'student', type: 'noun', cathegory: '', hint: 'Someone who is studying at a school or university', length: '7'},
-  9: {word: 'sunrise', type: 'noun', cathegory: '', hint: 'The time when the sun appears in the morning and the sky becomes light', length: '7'},
-  10: {word: 'fashion', type: 'noun', cathegory: '', hint: 'The most popular style of clothes or behaviour at a particular time', length: '7'}
+  1: {word: 'economy', type: 'noun', cathegory: '', hint: 'The system by which a country makes and uses goods and money', length: '7', displayed: false},
+  2: {word: 'process', type: 'noun', cathegory: '', hint: 'A series of actions that you take in order to do something', length: '7', displayed: false},
+  3: {word: 'premium', type: 'adjective', cathegory: '', hint: 'Used to refer to something that is of higher than usual quality', length: '7', displayed: false},
+  4: {word: 'extreme', type: 'adjective', cathegory: '', hint: 'At the furthest point of something', length: '7', displayed: false},
+  5: {word: 'alcohol', type: 'noun', cathegory: '', hint: 'Drinks such as wine and beer that can make you drunk', length: '7', displayed: false},
+  6: {word: 'context', type: 'noun', cathegory: '', hint: 'All the facts, opinions, etc. relating to a particular thing or event', length: '7', displayed: false},
+  7: {word: 'fitness', type: 'noun', cathegory: '', hint: 'The condition of being physically strong and healthy', length: '7', displayed: false},
+  8: {word: 'student', type: 'noun', cathegory: '', hint: 'Someone who is studying at a school or university', length: '7', displayed: false},
+  9: {word: 'sunrise', type: 'noun', cathegory: '', hint: 'The time when the sun appears in the morning and the sky becomes light', length: '7', displayed: false},
+  10: {word: 'fashion', type: 'noun', cathegory: '', hint: 'The most popular style of clothes or behaviour at a particular time', length: '7', displayed: false},
 }
 
 let eightLetter = {
-  1: {word: 'relation', type: 'noun', cathegory: '', hint: 'A connection or link between people or things', length: '8'},
-  2: {word: 'research', type: 'noun', cathegory: '', hint: 'The study of a subject in order to discover new information', length: '8'},
-  3: {word: 'colorful', type: 'adjective', cathegory: '', hint: 'Having bright colors', length: '8'},
-  4: {word: 'sentence', type: 'noun', cathegory: '', hint: 'A group of words, usually containing a verb, that expresses a complete idea', length: '8'},
-  5: {word: 'delivery', type: 'noun', cathegory: '', hint: 'The act of taking letters or things to a person or place', length: '8'},
-  6: {word: 'feedback', type: 'noun', cathegory: '', hint: 'Information or opinions about how well you have done', length: '8'},
-  7: {word: 'magazine', type: 'noun', cathegory: '', hint: 'A big, thin book that you can buy every week or month, that has pictures and writing', length: '8'},
-  8: {word: 'discount', type: 'noun', cathegory: '', hint: 'A reduction in the price of something', length: '8'},
-  9: {word: 'director', type: 'noun', cathegory: '', hint: 'An important manager in an organization or company', length: '8'},
-  10: {word: 'teenager', type: 'noun', cathegory: '', hint: 'Someone who is between 13 and 19 years old', length: '8'}   
+  1: {word: 'relation', type: 'noun', cathegory: '', hint: 'A connection or link between people or things', length: '8', displayed: false},
+  2: {word: 'research', type: 'noun', cathegory: '', hint: 'The study of a subject in order to discover new information', length: '8', displayed: false},
+  3: {word: 'colorful', type: 'adjective', cathegory: '', hint: 'Having bright colors', length: '8', displayed: false},
+  4: {word: 'sentence', type: 'noun', cathegory: '', hint: 'A group of words, usually containing a verb, that expresses a complete idea', length: '8', displayed: false},
+  5: {word: 'delivery', type: 'noun', cathegory: '', hint: 'The act of taking letters or things to a person or place', length: '8', displayed: false},
+  6: {word: 'feedback', type: 'noun', cathegory: '', hint: 'Information or opinions about how well you have done', length: '8', displayed: false},
+  7: {word: 'magazine', type: 'noun', cathegory: '', hint: 'A big, thin book that you can buy every week or month, that has pictures and writing', length: '8', displayed: false},
+  8: {word: 'discount', type: 'noun', cathegory: '', hint: 'A reduction in the price of something', length: '8', displayed: false},
+  9: {word: 'director', type: 'noun', cathegory: '', hint: 'An important manager in an organization or company', length: '8', displayed: false},
+  10: {word: 'teenager', type: 'noun', cathegory: '', hint: 'Someone who is between 13 and 19 years old', length: '8', displayed: false},
 }
 
 let nineLetter = {
-  1: {word: 'transform', type: 'verb', cathegory: '', hint: 'To change something completely, usually to improve it', length: '9'},
-  2: {word: 'important', type: 'adjective', cathegory: '', hint: 'Valuable, useful, or necessary', length: '9'},
-  3: {word: 'operation', type: 'noun', cathegory: '', hint: 'The process of cutting someone’s body to remove or repair part of it', length: '9'},
-  4: {word: 'crosswalk', type: 'noun', cathegory: '', hint: 'A special place on a road where traffic must stop to allow people to walk across', length: '9'},
-  5: {word: 'knowledge', type: 'noun', cathegory: '', hint: 'Information and understanding that you have in your mind', length: '9'},
-  6: {word: 'authority', type: 'noun', cathegory: '', hint: 'The official power to make decisions or control people', length: '9'},
-  7: {word: 'exception', type: 'noun', cathegory: '', hint: 'Someone or something that is not included in a rule, group, or list', length: '9'},
-  8: {word: 'confusion', type: 'noun', cathegory: '', hint: 'A state in which people do not understand what is happening or what they should do', length: '9'},
-  9: {word: 'detective', type: 'noun', cathegory: '', hint: 'Someone whose job is to discover information about a crime', length: '9'},
-  10: {word: 'vegetable', type: 'noun', cathegory: '', hint: 'A plant that you eat, for example a potato, onion, etc.', length: '9'}
+  1: {word: 'transform', type: 'verb', cathegory: '', hint: 'To change something completely, usually to improve it', length: '9', displayed: false},
+  2: {word: 'important', type: 'adjective', cathegory: '', hint: 'Valuable, useful, or necessary', length: '9', displayed: false},
+  3: {word: 'operation', type: 'noun', cathegory: '', hint: 'The process of cutting someone’s body to remove or repair part of it', length: '9', displayed: false},
+  4: {word: 'crosswalk', type: 'noun', cathegory: '', hint: 'A special place on a road where traffic must stop to allow people to walk across', length: '9', displayed: false},
+  5: {word: 'knowledge', type: 'noun', cathegory: '', hint: 'Information and understanding that you have in your mind', length: '9', displayed: false},
+  6: {word: 'authority', type: 'noun', cathegory: '', hint: 'The official power to make decisions or control people', length: '9', displayed: false},
+  7: {word: 'exception', type: 'noun', cathegory: '', hint: 'Someone or something that is not included in a rule, group, or list', length: '9', displayed: false},
+  8: {word: 'confusion', type: 'noun', cathegory: '', hint: 'A state in which people do not understand what is happening or what they should do', length: '9', displayed: false},
+  9: {word: 'detective', type: 'noun', cathegory: '', hint: 'Someone whose job is to discover information about a crime', length: '9', displayed: false},
+  10: {word: 'vegetable', type: 'noun', cathegory: '', hint: 'A plant that you eat, for example a potato, onion, etc.', length: '9', displayed: false},
 }
 
 /* VARIABLES CONTAINING THE OBJECTS OF WORDS BY LENGTH */
@@ -90,3 +90,7 @@ let wordsInPlay = {
   8: eightLetter,
   9: nineLetter
 }
+
+/* EXPORTS */
+
+export { fourLetter, fiveLetter, sixLetter, sevenLetter, eightLetter, nineLetter, wordsInPlay };
