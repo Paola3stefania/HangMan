e0d207a1b0a3e65452340633a58a2c0dfce18d97
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-07-15 09:01:20 +0200

    Keyboard and word functionalities & keyboard HTML test & words database

---
+++ assets/js/keyboard.js
@@ -0,0 +1,25 @@
+const KEYS = document.querySelectorAll('[data-letter]'); // We select all elements by the attribute as a NodeList.
+
+KEYS.forEach((key) => { // Add an event listener for each key button
+  key.addEventListener('click', buttonPressed)
+})
+
+/* We evaluate every keyboard event and, if it's a letter, we launch our functions: */
+document.addEventListener('keydown', (event) => { // ! CARE WITH ALT, TAB, ETC.
+  let letter = event.key;
+  let regExpTest = /[a-z]+/g;
+  regExpTest.test(letter) ? lookForLetter(letter) : "";
+  blockLetter(letter);
+})
+
+function blockLetter(letter) {
+  for (let i = 0; i < KEYS.length; i++) {
+    let test = (KEYS[i].dataset.letter === letter) ? KEYS[i].setAttribute("disabled", "disabled") : "";
+  }
+}
+
+function buttonPressed() {
+  this.setAttribute("disabled", "disabled"); // We block a new interaction with the letter button
+  let letter = this.dataset.letter;
+  lookForLetter(letter);
+}
---
+++ assets/js/letter.js
@@ -0,0 +1,32 @@
+let testWord = "pepinillo";
+
+function lookForLetter(letter) {
+  let correct = 0;
+  for (let i = 0; i < testWord.length; i++) {
+    let test = letter.includes(testWord[i]);
+    correct = (test === true) ? correct + 1 : correct + 0;
+  }
+  result = (correct > 0) ? correctLetter(letter) : wrongLetter(letter);
+}
+
+// FUNCIONALIDADES:
+// TODO REDONDEAR LA LETRA CORRECTA EN KEYBOARD
+// TODO MOSTRAR LAS LETRAS COINCIDENTES EN LA PALABRA JUGABLE
+// TODO METER LA LETRA EN UN ARRAY POR SI QUEREMOS HACERLE UN DISPLAY DE LETRAS USADAS
+// TODO SUMAR PUNTUACIÓN?
+function correctLetter(letter) {
+  console.log("correct "+letter);
+}
+
+let currentErrors = 0;
+
+// FUNCIONALIDADES:
+// // AÑADIR UN ERROR
+// TODO TACHAR LETRA INCORRECTA EN KEYBOARD
+// TODO METER LA LETRA EN UN ARRAY POR SI QUEREMOS HACERLE UN DISPLAY DE LETRAS USADAS
+// TODO AÑADIR UN ERROR + LANZAR FUNCIÓN CÁLCULO ERRORES
+// TODO RESTAR PUNTUACIÓN?
+function wrongLetter(letter) {
+  currentErrors += 1;
+  console.log("wrong "+letter);
+}
